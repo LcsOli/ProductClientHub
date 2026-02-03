9d582566a26ae7448a00aa8d0e47e63c390f22ef
lucasoli <lucasantonio97@live.com>
2026-02-03 15:10:01 -0300

    feat: iniciando a comunicação da API

---
+++ ProdutClientHub.API/Controller/ClientsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductsClientHub.Communication.Requests;
+using ProductsClientHub.Communication.Responses;
+
+namespace ProdutClientHub.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseClientJson), StatusCodes.Status201Created)]
+        public IActionResult Register([FromBody]RequestClientJson request)
+        {
+            var useCase = new ResponseClientJson();
+
+
+             var response = useCase.Execute(request);
+
+            return Created();
+        }
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok("Client updated successfully.");
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Retrieved all clients.");
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetbyId([FromRoute]Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok("Client deleted successfully.");
+        }
+    }
+}
---
+++ ProdutClientHub.API/UseCases/Clients/Register/RegisterClientUseCase.cs
@@ -0,0 +1,13 @@
+﻿using ProductsClientHub.Communication.Requests;
+using ProductsClientHub.Communication.Responses;
+
+namespace ProdutClientHub.API.UseCases.Clients.Register
+{
+    public class RegisterClientUseCase
+    {
+        public ResponseClientJson Execute(RequestClientJson request)
+        {
+          return new ResponseClientJson();
+        }
+    }
+}
