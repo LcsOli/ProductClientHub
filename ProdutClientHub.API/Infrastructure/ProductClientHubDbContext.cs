7d1676415747d82459d6ec0ebf210f5e7dadf609
lucasoli <lucasantonio97@live.com>
2026-03-17 13:01:10 -0300

    feat: incluindo conexão com banco sqlite

---
+++ ProdutClientHub.API/Infrastructure/ProductClientHubDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProdutClientHub.API.Entities;
+
+namespace ProdutClientHub.API.Infrastructure
+{
+    public class ProductClientHubDbContext : DbContext
+
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=productclienthub.sqlite");
+        }
+    }
+}
